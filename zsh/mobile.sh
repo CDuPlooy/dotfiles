b0a669ad0f08d8b97e23b30a0fd9c9b4476a141f
ioctl <armandconnor.duplooy@gmail.com>
2020-01-06 11:57:37 +0200

    added fetch_apk

--- zsh/mobile.sh
+++ zsh/mobile.sh
@@ -1,3 +1,10 @@
+# TODO: Modify this function to strip CRLF and only return LF
 function lpg(){
 	adb shell pm list packages | grep $1 -m 1 | cut -d ":" -f2
+}
+
+function fetch_apk(){
+	apk_path=$(adb shell pm path $(lpg $1 | tr -d "\r" | tr -d "\n") | cut -d ":" -f2)
+	apk_path=$(echo -n $apk_path | tr -d "\r" | tr -d "\n")
+	adb pull $apk_path $2
 }
